626ddd7c8714d2e210e25ad60e45e4bfe10d536d
krauson <40236466+krauson@users.noreply.github.com>
2020-12-10 12:39:01 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, render_template, request
+from riddles import get_riddle, get_list_from_str, format_str
+from models import database
+from pwe_curd import (create_user, create_table, check_username_password,
+                        add_points, get_users_scores)
+import random
+
+# todo
+# 1. bug in answer with more than one word, correct_answer from dict returns only one word and not the 
+#     whole sentence, but why?
+# 2. decorate.
+# 3. upload to heroku.
+# 4. erase print for debug
+
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+
+@app.route('/main')
+def main():
+    return render_template('index.html')
+
+
+@app.route('/riddles')
+def riddles():
+    str1 = 'hello my name is hagai'
+    former_correct_answer = request.args.get('correct_answer')
+    msg = None
+    if former_correct_answer != None:
+        former_possible_answers = request.args.get('possible_answers')
+        former_possible_answers = get_list_from_str(former_possible_answers)
+        for i in range(len(former_possible_answers)):
+            former_possible_answers[i] = format_str(former_possible_answers[i])
+        chosen_num = request.args.get('user_answer')
+        chosen_num = int(chosen_num) -1
+        user_answer = former_possible_answers[chosen_num]
+        if former_correct_answer == user_answer:
+            username = request.args.get('username')
+            add_points(username)
+            msg = '{} was a correct answer! you earned 5 points:)'
+            msg = msg.format(former_correct_answer)
+        else:
+            msg='Wrong answer.. the correct answer was: {}'
+            msg = msg.format(former_correct_answer)
+
+    riddle = get_riddle()
+    question = riddle['question']
+    question = format_str(question)
+    username = request.args.get('username')
+    possible_answers = riddle['incorrect_answers']
+    possible_answers.append(riddle['correct_answer'])
+    for i in range(len(possible_answers)):
+        possible_answers[i] = format_str(possible_answers[i])
+
+    correct_answer = riddle['correct_answer'] 
+    correct_answer = format_str(correct_answer)
+
+    random.shuffle(possible_answers)
+
+    print(f'current correct answer: {correct_answer}')
+
+    return render_template('riddles_page.html', msg=msg, possible_answers=possible_answers,
+    correct_answer=correct_answer, question=question, username=username, str1=str1)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    create_table()
+    print('username:')
+    print(request.form.get('username'))
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username is None:
+        return render_template('register.html')
+    else:
+        is_added = create_user(username, password)
+        print(f'the answer is {is_added}')
+        if bool(is_added) == True:
+            user_status = 'Welcome you are user number {} in our website!'.format(is_added)
+        else:
+            user_status = 'Error! The name {} is already taken.'.format(username)
+
+        print(f'is valid :{is_added}')
+        return render_template('register.html',username=username, user_status=user_status, is_added=is_added)
+
+
+@app.before_request
+def _db_connect():
+    database.connect()
+
+
+@app.teardown_request
+def _db_close(_):
+    if not database.is_closed():
+        database.close()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print('Im in login function')
+    username = request.form.get('username')
+    print(username)
+    if username is None:
+        return render_template('login.html')
+    else:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if check_username_password(username, password):
+            msg = 'welcome {}, so good to see you again:)'.format(username)
+            is_valid = True
+            return render_template('login.html', username=username,
+             msg=msg, is_valid = is_valid)
+        else:
+            msg = 'Error! username or password incorrect'
+            return render_template('login.html', msg=msg)
+
+
+@app.route('/scores')
+def show_score_table():
+    print('showing scores table!')
+    users = get_users_scores()
+    username = request.args.get('username')
+    return render_template('scores_table.html', users=users, username=username)
+
+if __name__ == '__main__':
+    app.run()
