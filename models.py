c032ed15f097977e252d2f1898347c54a1f8c6f9
Hagai <krauskas@gmail.com>
2020-12-12 18:23:43 +0200

    models

--- models.py
+++ models.py
@@ -9,6 +9,19 @@ database = PostgresqlDatabase(
     host=os.getenv('HOST'),
     port=os.getenv('MYPORT')
 )
+# os.getenv('DATABASE_URL'),
+# user=os.getenv('USER'),
+# password=os.getenv('PASSWORD'),
+# host=os.getenv('HOST'),
+# port=os.getenv('MYPORT')
+
+# print(os.getenv('DATABASE_URL'))
+# print(os.getenv('DATABASE_NAME'))
+# print(user)
+# print(password)
+# print(host)
+# print(port)
+
 
 
 class UnknownField(object):
